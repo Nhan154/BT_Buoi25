9c1d79d8d219e96cb66c79b240a2a27e81a1460c
Nhan154 <quangkhodo61@gmail.com>
2025-05-15 17:48:15 +0700

    Sửa code lần 1

--- vite-project/src/BT_glasses/ChangeGlasses.jsx
+++ vite-project/src/BT_glasses/ChangeGlasses.jsx
@@ -1,77 +1,77 @@
 import React, { useState } from "react";
 
+// Thành phần cần thay đổi: Glass
 const ChangeGlasses = () => {
-  const [glass, setGlass] = useState("./image/glassesImage/v1.png");
-  const handleChangeGlasses = (glass) => {
-    let image = `./image/glassesImage/v-${glass}.png`;
-    setGlass(image);
-  };
-
   const glassesList = [
     {
       id: 1,
       price: 30,
       name: "GUCCI G8850U",
-      url: "./glassesImage/v1.png",
+      url: "./image/glassesImage/v/v1.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
     {
       id: 2,
       price: 50,
       name: "GUCCI G8759H",
-      url: "./glassesImage/v2.png",
+      url: "./image/glassesImage/v/v2.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
     {
       id: 3,
       price: 30,
       name: "DIOR D6700HQ",
-      url: "./glassesImage/v3.png",
+      url: "./image/glassesImage/v/v3.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
     {
       id: 4,
       price: 70,
       name: "DIOR D6005U",
-      url: "./glassesImage/v4.png",
+      url: "./image/glassesImage/v/v4.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
     {
       id: 5,
       price: 40,
       name: "PRADA P8750",
-      url: "./glassesImage/v5.png",
+      url: "./image/glassesImage/v/v5.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
     {
       id: 6,
       price: 60,
       name: "PRADA P9700",
-      url: "./glassesImage/v6.png",
+      url: "./image/glassesImage/v/v6.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
     {
       id: 7,
       price: 80,
       name: "FENDI F8750",
-      url: "./glassesImage/v7.png",
+      url: "./image/glassesImage/v/v7.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
     {
       id: 8,
       price: 100,
       name: "FENDI F8500",
-      url: "./glassesImage/v8.png",
+      url: "./image/glassesImage/v/v8.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
     {
       id: 9,
       price: 60,
       name: "FENDI F4300",
-      url: "./glassesImage/v9.png",
+      url: "./image/glassesImage/v/v9.png",
       desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
     },
   ];
+
+  const [selectedGlasses, setSelectedGlasses] = useState(glassesList[0]);
+  const handleChangeGlasses = (glass) => {
+    setSelectedGlasses(glass);
+  };
 
   return (
     <div
@@ -88,33 +88,33 @@ const ChangeGlasses = () => {
 
       <div className="row">
         <div className="row justify-content-center align-items-start my-5">
-          <div className="col-md-3 position-relative text-center mx-4">
+          <div className="col-md-3 position-relative mx-4">
             <img
               src="./image/glassesImage/model.jpg"
               alt="Model with glasses"
               className="img-fluid"
             />
-            {/* {selectedGlasses && (
+            {selectedGlasses && selectedGlasses.url && (
               <>
                 <img
                   src={selectedGlasses.url}
                   alt="Glasses"
                   className="position-absolute"
                   style={{
-                    top: "100px",
-                    left: "35px",
+                    top: "105px",
+                    left: "100px",
                     width: "180px",
                     opacity: 0.9,
                   }}
                 />
-                <div className="position-absolute bottom-0 start-0 end-0 bg-warning bg-opacity-75 text-dark p-2">
+                <div className="position-absolute bottom-0 start-0 end-0 bg-warning bg-opacity-50 text-dark" style={{padding: '30px', transform: 'translateX(3%)', width: '94%' }}>
                   <h6 className="text-primary fw-bold m-0">
                     {selectedGlasses.name}
                   </h6>
                   <small>{selectedGlasses.desc}</small>
                 </div>
               </>
-            )} */}
+            )}
           </div>
 
           <div className="col-md-3 text-center mx-4">
@@ -126,41 +126,31 @@ const ChangeGlasses = () => {
           </div>
         </div>
 
-
-
         <div className="container bg-light rounded shadow p-4 mb-5">
-          <button
-            className="btn"
-            style={{ backgroundImage: "./image/glassesImage/v1.jpg" }}
-            onClick={() => {
-              // setCar("./images/CarBasic/products/red-car.jpg")
-              handleChangeGlasses("1");
-            }}
-          ></button>
-          <button
-            className="btn"
-            style={{ backgroundImage: "./image/glassesImage/v2.jpg" }}
-            onClick={() => {
-              // setCar("./images/CarBasic/products/steel-car.jpg")
-              handleChangeGlasses("2");
-            }}
-          ></button>
-          <button
-            className="btn"
-            style={{ backgroundImage: "./image/glassesImage/v3.jpg" }}
-            onClick={() => {
-              // setCar("./images/CarBasic/products/black-car.jpg")
-              handleChangeGlasses("3");
-            }}
-          ></button>
-          <button
-            className="btn"
-            style={{ backgroundImage: "./image/glassesImage/v4.jpg" }}
-            onClick={() => {
-              // setCar("./images/CarBasic/products/silver-car.jpg")
-              handleChangeGlasses("4");
-            }}
-          ></button>
+          <div className="row row-cols-2 row-cols-sm-3 row-cols-md-6 g-3">
+           
+            {glassesList.map((glass) => (
+              <div className="col text-center" key={glass.id}>
+                <button
+                  onClick={() => handleChangeGlasses(glass)}
+                  className="btn p-1 border border-secondary bg-white w-100"
+                  style={{ height: "100px" }}
+                >
+                  {glass.url ? (
+                    <img
+                      src={glass.url}
+                      alt={glass.name}
+                      className="img-fluid"
+                    />
+                  ) : (
+                    <div className="w-100 h-100 d-flex justify-content-center align-items-center text-muted">
+                      No Image
+                    </div>
+                  )}
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
